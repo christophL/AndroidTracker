df154d530639dd5f4b12b3ed26c3644bd3e1dfe0
Christoph Leitner <c.leitner@student.uibk.ac.at>
2015-01-26 11:18:31 +0100

    added install script for web app

---
+++ infsecApp/install.sh
@@ -0,0 +1,3 @@
+sudo cp -r * /opt/lampp/htdocs/infsecApp/
+sudo mv /opt/lampp/htdocs/infsecApp/server.crt /opt/lampp/etc/ssl.crt
+sudo mv /opt/lampp/htdocs/infsecApp/server.key /opt/lampp/etc/ssl.key
