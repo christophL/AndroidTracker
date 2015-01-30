534660232f5cad28204ccc1368c0d8a657504df0
Christoph Leitner <c.leitner@student.uibk.ac.at>
2015-01-30 16:40:09 +0100

    updated readme and updated install script to generate a new server certificate, if not already present

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 androidTracker
 ==============
+
+##Webapp-installation (with XAMPP)
+1. Download and install XAMPP (e.g., from https://www.apachefriends.org/download.html)
+2. Run the install script in the infsecApp folder (assumes your xampp installation is in /opt/lampp, if it is not you need to adapt the script to your actual install location)
+
+
+##Android-app
+* The project can be imported and built using android-studio 
--- infsecApp/install.sh
+++ infsecApp/install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 sudo cp -r * /opt/lampp/htdocs/infsecApp/
-sudo mv /opt/lampp/htdocs/infsecApp/server.crt /opt/lampp/etc/ssl.crt
-sudo mv /opt/lampp/htdocs/infsecApp/server.key /opt/lampp/etc/ssl.key
+
+if [ ! -f server.key ]; then
+    openssl req -new -x509 -days 365 -sha1 -newkey rsa:2048 -nodes -keyout server.key -out server.crt
+fi
+
+sudo mv server.crt /opt/lampp/etc/ssl.crt
+sudo mv server.key /opt/lampp/etc/ssl.key
