29cc921bac0b66e2f51d97d159a0f9d5709c1fd1
Christoph Leitner <c.leitner@student.uibk.ac.at>
2015-01-31 22:24:09 +0100

    key cert from install script should be copied, not moved

--- infsecApp/install.sh
+++ infsecApp/install.sh
@@ -5,5 +5,5 @@ if [ ! -f server.key ]; then
     openssl req -new -x509 -days 365 -sha1 -newkey rsa:2048 -nodes -keyout server.key -out server.crt
 fi
 
-sudo mv server.crt /opt/lampp/etc/ssl.crt
-sudo mv server.key /opt/lampp/etc/ssl.key
+sudo cp server.crt /opt/lampp/etc/ssl.crt
+sudo cp server.key /opt/lampp/etc/ssl.key
